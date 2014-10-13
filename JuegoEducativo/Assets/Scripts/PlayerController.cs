4a1c22b0466876d01e05d7107fc067fd242a1172
Sandra Lopez <sandramlopez2014@gmail.com>
2014-10-12 19:25:12 -0500

    Iniciando el juego
    Se agrega el camión y los bocetos iniciales.
    El boceto inicial del personaje tienen movimiento.

---
+++ JuegoEducativo/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float velocidad = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Movimiento
+		if (Input.GetKey (KeyCode.RightArrow)) 
+		{
+			transform.Translate(new Vector3(velocidad * Time.deltaTime,0,0));
+		}
+		
+		if (Input.GetKey (KeyCode.LeftArrow)) 
+		{
+			transform.Translate(new Vector3(-velocidad * Time.deltaTime,0,0));
+		}
+		
+		else if (!Input.anyKey){
+		}
+
+	
+	}
+}
